dd407889856e9b0e86d1c16eb38c48766bd86cc8
cryptoswarm <mokhtar.safir@outlook.com>
2020-09-04 18:11:56 -0400

    emplyee enterprise

---
+++ src/Employee.java
@@ -0,0 +1,124 @@
+public class Employee {
+    /**
+     * Pour chaque employé, vous devez afficher les informations dans l’ordre suivant
+     * sur une seule ligne avec une barre oblique (|) comme séparateur :
+
+
+
+     * le taux de l’augmentation de salaire,
+     * l'augmentation de salaire
+     * et le salaire après augmentation.
+     */
+    private  String employeeRegistration;
+    private String employeeLastName;
+    private String employeeFirstName;
+    private int employeeEchelon;
+    private double employeeOldSalary;
+    private char performanceGrade;
+    private String performanceDescription;
+    private float bonusRate;
+    private double bonus;
+    private float salaryRaiseRate;
+    private double salaryRaiseAmount;
+    private double newSalary;
+
+    public Employee( String employeeRegistration, String employeeLastName, String employeeFirstName,
+                     int employeeEchelon, double employeeOldSalary, char performanceGrade){
+        this.employeeRegistration = employeeRegistration;
+        this.employeeLastName = employeeLastName;
+        this.employeeFirstName = employeeFirstName;
+        this.employeeEchelon = employeeEchelon;
+        this.employeeOldSalary = employeeOldSalary;
+        this.performanceGrade = performanceGrade;
+
+    }
+
+    public String getEmployeeRegistration(){
+        return employeeRegistration;
+    }
+
+    public String getEmployeeLastName(){
+        return employeeLastName;
+    }
+    public String getEmployeeFirstName(){
+        return employeeFirstName;
+    }
+
+    public int getEmployeeEchelon(){
+        return employeeEchelon;
+    }
+
+    public double getEmployeeOldSalary(){
+        return employeeOldSalary;
+    }
+
+    public char getPerformanceGrade(){
+        return performanceGrade;
+    }
+
+    public String getPerformanceDescription(){
+        return performanceDescription;
+    }
+
+    public float getBonusRate(){
+        return bonusRate;
+    }
+
+    public double getBonus(){
+        return bonus;
+    }
+
+    public float getSalaryRaiseRate(){
+        return salaryRaiseRate;
+    }
+    public double getSalaryRaiseAmount(){
+        return salaryRaiseAmount;
+    }
+    public double getNewSalary(){
+        return newSalary;
+    }
+
+    public void setPerformanceDescription(String performanceDescription){
+        this.performanceDescription = performanceDescription;
+    }
+    public void setBonusRate(float bonusRate){
+        this.bonusRate = bonusRate;
+    }
+
+    public void setBonus(double bonus){
+        this.bonus = bonus;
+    }
+
+    public void setSalaryRaiseRate(float salaryRaiseRate){
+        this.salaryRaiseRate = salaryRaiseRate;
+    }
+
+    public void setSalaryRaiseAmount(double salaryRaiseAmount){
+        this.salaryRaiseAmount = salaryRaiseAmount;
+    }
+    public void setNewSalary(double newSalary){
+        this.newSalary = newSalary;
+    }
+
+    public  String toString(){
+        return String.format("|%s|%s %s| %d| %6.2f | %s | %2.1f | %5.2f | %1.1f | %5.2f | %6.2f |", employeeRegistration,
+                employeeLastName, employeeFirstName, employeeEchelon,  employeeOldSalary,
+                performanceDescription, bonusRate, bonus, salaryRaiseRate, salaryRaiseAmount, newSalary );
+
+    }
+
+    public void display() {
+        System.out.printf("Employee registration : %s\n", employeeRegistration);
+        System.out.printf("Employee last and first name  : %s %s\n",employeeLastName, employeeFirstName);
+        System.out.printf("Employee Echelon : %d \n",employeeEchelon);
+        System.out.printf("Employee old salary  : %6.2f\n", employeeOldSalary);
+        System.out.printf("Employee performance description : %s\n", performanceDescription);
+        System.out.printf("Employee bonus rate : %2.2f\n", bonusRate);
+        System.out.printf("Employee bonus : %2.2f\n",bonus);
+        System.out.printf("Employee raise rate  : %2.2f\n", salaryRaiseRate);
+        System.out.printf("Employee salary raise amount  : %6.2f\n", salaryRaiseAmount);
+        System.out.printf("Employee new salary without bonus : %6.2f\n", newSalary);
+
+    }
+    
+}
---
+++ src/Principal.java
@@ -0,0 +1,296 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Principal {
+    final static String MSG_WELCOME = "Welcome in Entrprise Employe App.\n The app will help you calculate bonus and salary";
+    final static String MSG_QUIT = "Thank you for using this app.";
+    final static String MAIN_MENU = "0. Quit\n"+
+                                    "1. Salary and bonus raise estimation \n"+
+                                    //"2. Salary raise estimation\n"+
+                                    "2. Display all employees\n";
+    final static String MSG_PROMPT_MENU_CHOICE = "What is your choice ? Please enter either 0, 1, or 2 : ";
+    final static String MSG_ERR_USER_INPUT_MENU_CHOICE = "Wrong menu choice! Please enter either 0, 1, 2 or 3 : ";
+    final static String MSG_PROMPT_USER_REGISTRATION_NBR = "Enter your REGISTRATION nbr respecting the following format\n" +
+                                                            "A valid registration number : abcd12 or ABCD12 : ";
+    final static String MSG_ERR_PROMPT_USER_REGISTRATION_NBR = "Wrong registration nbr!";
+    final static String MSG_PROMPT_EMPLOYE_LAST_NAME = "Enter your last name : ";
+    final static String MSG_ERR_EMPLOYE_LAST_NAME = "Employe last name is invalid!";
+    final static String MSG_PROMPT_EMPLOYE_FIRST_NAME = "Enter your first name :";
+    final static String MSG_ERR_EMPLOYE_FIRST_NAME = "Employe first name is invalid!";
+    final static String MSG_PROMPT_EMPLOYEE_ECHELON = "Enter employee echelon. 1, 2 or 3 : ";
+    final static String MSG_ERR_EMPLOYEE_ECHELON = "Invalid employee echelon";
+    final static String MSG_PROMPT_EMPLOYE_SALARY = "Enter employe salary : ";
+    final static String MSG_ERR_EMPLOYE_SALARY = "Employe salary is invalid! Should be superieur than 0";
+    final static String MSG_PROMPT_EMPLOYEE_PERFORMANCE_GRADE = "Enter employee performance grade. Should be D, A, P, or N : ";
+    final static String MSG_ERR_EMPLOYEE_PERFORMANCE_GRADE = "Employee performance grade is invalid";
+
+    /**
+     * Print welcom msg
+     */
+    public static void printMsgWelcom(){
+        System.out.println(MSG_WELCOME);
+    }
+
+    public static void printQuitMsg(){
+        System.out.println(MSG_QUIT);
+    }
+    /**
+     * Affichage du menu de choix
+     */
+    public static void  printMenu(){
+        System.out.println(MAIN_MENU);
+    }
+
+    /**
+     * Ask for user choice from main menu.
+     * Check if choice is valid.
+     * @return user main menu choice.
+     */
+    public static int checkUserChoiceInput(){
+        int input = 0;
+        boolean isValid ;
+        do {
+            isValid = true;
+            System.out.print(MSG_PROMPT_MENU_CHOICE);
+            try {
+                Scanner sc = new Scanner(System.in);
+                input = sc.nextInt();
+                if (input != 0 && input != 1 && input != 2 && input != 3) {
+                    System.out.print(MSG_ERR_USER_INPUT_MENU_CHOICE);
+                    isValid = false;
+                }
+            }catch (InputMismatchException e ){
+                System.out.print(MSG_ERR_USER_INPUT_MENU_CHOICE);
+                isValid = false;
+            }
+        } while (!isValid);
+        return input;
+    }
+
+    /**
+     * Ask for employe registration
+     * Check employe registration
+     * @return Employe registration number
+     */
+    public static String checkUserRegistrationNbr(){
+        String registration = "";
+        boolean isValid ;
+        do{
+            isValid = true;
+            System.out.print( MSG_PROMPT_USER_REGISTRATION_NBR );
+            Scanner sc = new Scanner(System.in);
+            registration = sc.nextLine().toLowerCase();
+
+            if( registration.length() != 6 ) {
+                isValid = false;
+            }else{
+                for(int i=0; i< registration.length() ; i++){
+                    if(!Character.isLetter( registration.charAt(i) ) && i < 4 ){
+                        isValid = false;
+                        break;
+                    }
+                    if(Character.isLetter( registration.charAt(i) ) && i >= 4){
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+            if(!isValid){
+                System.out.println(MSG_ERR_PROMPT_USER_REGISTRATION_NBR);
+            }
+        }while(!isValid);
+        return registration;
+    }
+
+    /**
+     * Ask for employe last and first name
+     * Check employe last and first name
+     * @return last or first name
+     */
+    public static String checkEmployeeName(String askMsg, String errMsg){
+        String lastName;
+        boolean isValid;
+        do{
+            isValid = true;
+            System.out.print( askMsg );
+            Scanner sc = new Scanner(System.in);
+            lastName = sc.nextLine();
+            if(lastName.length() <2 ||  lastName.length() > 30){
+                isValid = false;
+            }else{
+                for(int i=0; i<lastName.length(); i++){
+                    if( !Character.isLetter(lastName.charAt(i)) ){
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+            if(!isValid){
+                System.out.println(errMsg);
+            }
+        }while(!isValid);
+        return  lastName;
+    }
+
+    /**
+     * Ask for employee echelon
+     * Check employee echelon is valid
+     * @return employee echelon
+     */
+    public static int checkEmployeeEchelon(){
+        int employeeEchelon = 0;
+        boolean isValid;
+        do{
+            isValid = true;
+            System.out.print(MSG_PROMPT_EMPLOYEE_ECHELON);
+            try {
+                Scanner sc = new Scanner(System.in);
+                employeeEchelon = sc.nextInt();
+                if(employeeEchelon < 1 || employeeEchelon > 3){
+                    System.out.println(MSG_ERR_EMPLOYEE_ECHELON);
+                    isValid = false;
+                }
+            }catch(InputMismatchException e){
+                System.out.println(MSG_ERR_EMPLOYEE_ECHELON);
+                isValid = false;
+            }
+        }while(!isValid);
+        return  employeeEchelon;
+    }
+
+    /**
+     * Ask for employee salary
+     * Check employee salary
+     * @return
+     */
+    public static double checkEmployeeSalary(){
+        double employeeSalary = 0.0;
+        boolean isValid;
+        do{
+            isValid = true;
+            System.out.print(MSG_PROMPT_EMPLOYE_SALARY);
+            try{
+                Scanner sc = new Scanner(System.in);
+                employeeSalary = sc.nextDouble();
+                if(employeeSalary <= 0 ){
+                    System.out.println(MSG_ERR_EMPLOYE_SALARY);
+                    isValid = false;
+                }
+            }catch(InputMismatchException e){
+                System.out.println(MSG_ERR_EMPLOYE_SALARY);
+                isValid = false;
+            }
+        }while(!isValid);
+        return employeeSalary;
+    }
+    public static char checkEmployeePerformanceGrade(){
+        char performanceGrade='X';
+        boolean isValid;
+        do{
+            isValid = true;
+            System.out.print(MSG_PROMPT_EMPLOYEE_PERFORMANCE_GRADE);
+            Scanner sc = new Scanner(System.in);
+            performanceGrade = sc.nextLine().charAt(0);
+            if(!Character.isLetter(performanceGrade)){
+                System.out.println(MSG_ERR_EMPLOYEE_PERFORMANCE_GRADE);
+                isValid = false;
+            }else if(performanceGrade !='D' && performanceGrade !='A' && performanceGrade !='P' && performanceGrade !='N'){
+                System.out.println(MSG_ERR_EMPLOYEE_PERFORMANCE_GRADE);
+                isValid = false;
+            }
+
+        }while(!isValid);
+        return  performanceGrade;
+    }
+
+    public static void main(String[] args) {
+        String registration;
+        String lastName;
+        String firstName;
+        int echelon;
+        double currentSalary;
+        char performanceGrade;
+        double bonus;
+        double salaryRaiseAmount;
+
+        printMsgWelcom();
+        printMenu();
+        /*
+        int mainMenuChoice = checkUserChoiceInput();
+        System.out.println("Main menu choice = "+mainMenuChoice);
+
+        String employeeRegistration = checkUserRegistrationNbr();
+        System.out.println("Employe registration nbr = "+ employeeRegistration);
+
+        String lastName = checkEmployeeName(MSG_PROMPT_EMPLOYE_LAST_NAME, MSG_ERR_EMPLOYE_LAST_NAME);
+        System.out.println("Employe last name = "+ lastName );
+
+        String firstName = checkEmployeeName(MSG_PROMPT_EMPLOYE_FIRST_NAME, MSG_ERR_EMPLOYE_FIRST_NAME);
+        System.out.println("Employe first name = "+firstName  );
+
+        int employeeEchelon = checkEmployeeEchelon();
+        System.out.println("Employee echelon = "+employeeEchelon );
+
+        double oldEmployeeSalary = checkEmployeeSalary();
+        System.out.println("Employee salary = "+ oldEmployeeSalary);
+
+        char employeePerformanceGrade = checkEmployeePerformanceGrade();
+        System.out.println("Employee performance grade = "+ employeePerformanceGrade);
+
+        double bonusRate = BonusSalaryRaise.getBonusRate(employeeEchelon, employeePerformanceGrade);
+        System.out.println("Employee bonus rate = "+bonusRate);
+
+        float salaryRateRaise = BonusSalaryRaise.getRateSalaryRaise( employeeEchelon );
+        double newSalaryWithoutBonus = BonusSalaryRaise.getNewSalary(salaryRateRaise, oldEmployeeSalary);
+        System.out.println("New employee salary = "+newSalaryWithoutBonus);
+*/
+        Enterprise aCompany = new Enterprise();
+        aCompany.readFileOfEmployees();
+        while(true){
+
+            int choice = checkUserChoiceInput();
+            if(choice == 0){
+                printQuitMsg();
+                aCompany.saveEmployeesInFile();
+                break;
+            }else if( choice == 1){
+                /**
+                 * Choix 1 : permet de calculer le bonus et l’augmentation de salaire d’un employé.
+                 * Les données à saisir sont :
+                 * le matricule, le nom, le prénom, l’échelon, le salaire ,
+                 * et la note d’évaluation de la performance.
+                 */
+                registration = checkUserRegistrationNbr();
+                lastName = checkEmployeeName(MSG_PROMPT_EMPLOYE_LAST_NAME, MSG_ERR_EMPLOYE_LAST_NAME );
+                firstName =  checkEmployeeName(MSG_PROMPT_EMPLOYE_FIRST_NAME, MSG_ERR_EMPLOYE_FIRST_NAME);
+                echelon = checkEmployeeEchelon();
+                currentSalary = checkEmployeeSalary();
+                performanceGrade = checkEmployeePerformanceGrade();
+
+                Employee anEmployee = new Employee(registration, lastName, firstName, echelon, currentSalary, performanceGrade);
+
+                anEmployee.setPerformanceDescription( BonusSalaryRaise.displayPerformanceGradeDescription(performanceGrade) );
+
+                anEmployee.setSalaryRaiseRate( BonusSalaryRaise.getRateSalaryRaise(echelon) );
+                anEmployee.setSalaryRaiseAmount( BonusSalaryRaise.getRateSalaryRaise(echelon));
+                anEmployee.setBonusRate( BonusSalaryRaise.getBonusRate(echelon, performanceGrade) );
+                anEmployee.setBonus( BonusSalaryRaise.getEmployeeBonus(
+                                     BonusSalaryRaise.getBonusRate(echelon, performanceGrade), currentSalary) );
+
+                anEmployee.setNewSalary(BonusSalaryRaise.getNewSalary((float) anEmployee.getSalaryRaiseAmount(), currentSalary));
+
+                anEmployee.display();
+
+                aCompany.addEmployee(anEmployee);
+
+            }else if( choice == 2){
+                aCompany.printAllEmployeesData();
+
+            }
+
+        }
+
+
+    }
+
+}
