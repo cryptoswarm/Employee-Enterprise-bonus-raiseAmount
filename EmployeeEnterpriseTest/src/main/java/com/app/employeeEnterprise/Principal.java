cc3e5497c81b2cecd697715b907b5d7c69a9f8f4
cryptoswarm <mokhtar.safir@outlook.com>
2022-05-30 11:05:53 -0400

    removing .idea

--- EmployeeEnterpriseTest/src/main/java/com/app/employeeEnterprise/Principal.java
+++ EmployeeEnterpriseTest/src/main/java/com/app/employeeEnterprise/Principal.java
@@ -15,67 +15,12 @@ public class Principal {
 
     public static void main(String[] args) {
         SpringApplication.run(Principal.class, args);
-
-        /*String registration;
-        String lastName;
-        String firstName;
-        int echelon;
-        double currentSalary;
-        char performanceGrade;
-
-        Message.printMsgWelcom();
-        Message.printMenu();
-        IExtractor extractor = new Extractor();
-        ISaver saver = new Saver();
-        IChecker checker = new Checker();
-
-        IEnterprise aCompany = new Enterprise();
-        extractor.readFile("employeeInfoBefore.txt", new Creator(), aCompany);
-        while (true) {
-
-            int choice = checker.checkUserChoiceInput();
-            if (choice == 0) {
-                Message.printQuitMsg();
-                saver.saveEmployees(aCompany, "EmployeesDataAfterEdit");
-                saver.displayData("EmployeesDataAfterEdit");
-                break;
-            } else if (choice == 1) {
-
-                registration = checker.checkUserRegistrationNbr();
-                lastName = checker.checkEmployeeName(Constante.MSG_PROMPT_EMPLOYE_LAST_NAME, Constante.MSG_ERR_EMPLOYE_LAST_NAME);
-                firstName = checker.checkEmployeeName(Constante.MSG_PROMPT_EMPLOYE_FIRST_NAME, Constante.MSG_ERR_EMPLOYE_FIRST_NAME);
-                echelon = checker.checkEmployeeEchelon();
-                currentSalary = checker.checkEmployeeSalary();
-                performanceGrade = checker.checkEmployeePerformanceGrade();
-
-                IEmployee anEmployee = new Employee(registration, lastName, firstName, echelon, currentSalary, performanceGrade);
-
-                anEmployee.setPerformanceDescription(BonusSalaryRaise.displayPerformanceGradeDescription(performanceGrade));
-
-                anEmployee.setSalaryRaiseRate(BonusSalaryRaise.getRateSalaryRaise(echelon));
-                anEmployee.setSalaryRaiseAmount(BonusSalaryRaise.getRateSalaryRaise(echelon));
-                anEmployee.setBonusRate(BonusSalaryRaise.getBonusRate(echelon, performanceGrade));
-                anEmployee.setBonus(BonusSalaryRaise.getEmployeeBonus(
-                        BonusSalaryRaise.getBonusRate(echelon, performanceGrade), currentSalary));
-
-                anEmployee.setNewSalary(BonusSalaryRaise.getNewSalary((float) anEmployee.getSalaryRaiseAmount(), currentSalary));
-
-                anEmployee.display();
-
-                aCompany.addEmployee(anEmployee);
-
-            } else if (choice == 2) {
-                saver.displayData("EmployeesDataAfterEdit");
-            }
-
-        }*/
     }
 
     @Bean //whenever the app starts this bean will be pick up
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-
 
     @Bean
     CommandLineRunner run(EmployeeService employeeService)
